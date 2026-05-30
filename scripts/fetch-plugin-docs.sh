17e1e5756478a297af403916d2e8bc8e86451ee6
Felix Yeung <19355619+imfelixyeung@users.noreply.github.com>
2026-05-30 01:24:00 +0100

    fix: clone using https instead of ssh

--- scripts/fetch-plugin-docs.sh
+++ scripts/fetch-plugin-docs.sh
@@ -8,6 +8,6 @@ find $DOCS_DIR -depth -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 rm -rf
 
 mkdir -p $PLUGINS_DIR
 
-git clone --filter=blob:none --sparse git@github.com:moodlicious/moodle-local_shortlinks.git $PLUGINS_DIR/local_shortlinks
+git clone --filter=blob:none --sparse https://github.com/moodlicious/moodle-local_shortlinks.git $PLUGINS_DIR/local_shortlinks
 git -C $PLUGINS_DIR/local_shortlinks sparse-checkout set docs
 mv $PLUGINS_DIR/local_shortlinks/docs $DOCS_DIR/local_shortlinks
